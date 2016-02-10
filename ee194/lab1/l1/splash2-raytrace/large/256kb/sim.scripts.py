e8e1dc1f2fe22f899473834886734c0caa063b09
Riley Wood <rjw245@gmail.com>
2016-02-10 09:26:32 -0500

    Not hosting ee194 stuff anymore

--- ee194/lab1/l1/splash2-raytrace/large/256kb/sim.scripts.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.argv = [ "/h/rwood04/ee194/sniper/scripts/periodic-stats.py", "1000:2000" ]
-execfile("/h/rwood04/ee194/sniper/scripts/periodic-stats.py")
-sys.argv = [ "/h/rwood04/ee194/sniper/scripts/markers.py", "markers" ]
-execfile("/h/rwood04/ee194/sniper/scripts/markers.py")
