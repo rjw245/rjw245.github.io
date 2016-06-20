46ebd67277c37b278d8913db7c37e905601e8c80
Riley Wood <rjw245@gmail.com>
2016-06-20 17:40:17 -0400

    Removed deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git ls-remote --exit-code -q github > /dev/null 2>&1
-if test $? != 0; then
-    printf "Adding github remote\n"
-    git remote add github https://github.com/rjw245/rjw245.github.io.git
-fi
-git push github public:master
